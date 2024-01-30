b44a5cff2a660068019b9f327248aff8bd4de4ed
Liam Spedding <liam.spedding@creode.co.uk>
2024-01-30 09:33:54 +0000

    Install Hubspot API library

---
+++ src/Contracts/SubmissionInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Creode\LaravelHubspotForms\Contracts;
+
+interface SubmissionInterface
+{
+
+}
---
+++ src/LaravelHubspotAPIServiceProvider.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Creode\LaravelHubspotForms;
+
+class LaravelHubspotAPIServiceProvider
+{
+
+}
