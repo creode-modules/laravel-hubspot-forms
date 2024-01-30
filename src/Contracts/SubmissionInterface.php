a44c8a6f98b3485886753819c3b6847b93ddb553
Liam Spedding <liam.spedding@creode.co.uk>
2024-01-30 14:24:30 +0000

    Add method to interface and add validate method service

--- src/Contracts/SubmissionInterface.php
+++ src/Contracts/SubmissionInterface.php
@@ -5,5 +5,6 @@ namespace Creode\LaravelHubspotForms\Contracts;
 interface SubmissionInterface
 {
     public function update($user, $contactId);
+    public function createNote($contactId, $noteBody);
     public function find($field, $email);
 }
--- src/LaravelHubspotAPIServiceProvider.php
+++ src/LaravelHubspotAPIServiceProvider.php
@@ -4,12 +4,15 @@ namespace Creode\LaravelHubspotForms;
 
 use Carbon\Carbon;
 use Creode\LaravelHubspotForms\Contracts\SubmissionInterface;
+use http\Exception;
+use HubSpot\Client\Crm\Contacts\ApiException;
+use HubSpot\Client\Crm\Contacts\Model\Error;
+use HubSpot\Client\Crm\Contacts\Model\SimplePublicObject;
 use HubSpot\Client\Crm\Contacts\Model\SimplePublicObjectInput;
 use HubSpot\Client\Crm\Contacts\Model\Filter;
 use HubSpot\Client\Crm\Contacts\Model\FilterGroup;
 use HubSpot\Client\Crm\Contacts\Model\PublicObjectSearchRequest;
 use HubSpot\Client\Crm\Objects\Notes\Model\SimplePublicObjectInput as ActivityInput;
-use HubSpot\Client\Crm\Associations\Model\BatchInputPublicAssociation;
 use HubSpot\Factory;
 
 class LaravelHubspotAPIServiceProvider implements SubmissionInterface
@@ -19,9 +22,8 @@ class LaravelHubspotAPIServiceProvider implements SubmissionInterface
 
     protected array $contactFields = [
         'email',
-        'firstName',
-        'lastName',
-        'company',
+        'firstname',
+        'lastname',
     ];
 
     public function __construct()
@@ -29,22 +31,12 @@ class LaravelHubspotAPIServiceProvider implements SubmissionInterface
         $this->hubspot = Factory::createWithAccessToken(config('laravel-hubspot-forms.access_token'));
     }
 
-    public function update($user, $contactId)
-    {
-        $contactProperties = new SimplePublicObjectInput();
-
-        $contactProperties->setProperties($this->setContactFields($user));
-
-        return $this->hubspot->crm()->contacts()->basicApi()->update($contactId, $contactProperties);
-    }
-
     public function getOwners()
     {
-
         return $this->hubspot->crm()->owners()->getAll();
     }
 
-    public function createNote($contactId)
+    public function createNote($contactId, $noteBody)
     {
         $activityProperties = new ActivityInput();
 
@@ -52,7 +44,7 @@ class LaravelHubspotAPIServiceProvider implements SubmissionInterface
 
         $activityProperties->setProperties([
             'hs_timestamp' => Carbon::now('UTC'),
-            'hs_note_body' => 'New Wishlist created. <br> <a href="https://awdis.creode.co.uk/wishlist/wishlist-id-here">View Wishlist</a>',
+            'hs_note_body' => 'New Wishlist created: <a href="https://awdis.creode.co.uk/wishlist/wishlist-id-here">View Wishlist</a>',
             'hubspot_owner_id' => $ownerId,
         ]);
 
@@ -62,7 +54,23 @@ class LaravelHubspotAPIServiceProvider implements SubmissionInterface
             'method' => 'PUT',
             'path' => '/crm/v3/objects/notes/'.$note->getId().'/associations/contact/'.$contactId.'/202',
         ]);
+    }
 
+    /**
+     * @param  array  $user  Required fields are email, firstname, lastname
+     * @param  int  $contactId  The Hubspot ID of the contact the note should be associated to
+     * @return Error|SimplePublicObject
+     * @throws ApiException
+     */
+    public function update($user, $contactId)
+    {
+        $this->validate($user);
+
+        $contactProperties = new SimplePublicObjectInput();
+
+        $contactProperties->setProperties($this->setContactFields($user));
+
+        return $this->hubspot->crm()->contacts()->basicApi()->update($contactId, $contactProperties);
     }
 
     public function find($field, $email)
@@ -82,6 +90,24 @@ class LaravelHubspotAPIServiceProvider implements SubmissionInterface
         $searchRequest->setProperties($this->contactFields);
 
         return $this->hubspot->crm()->contacts()->searchApi()->doSearch($searchRequest);
+    }
+
+    // Validate fields
+    public function validate($data)
+    {
+        if (!$data) {
+            throw new \Exception('No data provided');
+        }
+
+        foreach ($this->contactFields as $field) {
+            if (!isset($data[$field])) {
+                throw new \Exception('Missing required field: '.$field);
+            }
+
+            if (!$data[$field]) {
+                throw new \Exception('Field '.$field.' is empty.');
+            }
+        }
     }
 
     protected function setContactFields($user)
