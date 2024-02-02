729cbf84633b5360c25c1681e66f5f41e972d60e
Liam Spedding <liam.spedding@creode.co.uk>
2024-02-02 16:55:11 +0000

    Implement new excepitons and add required fields to config file

--- config/laravel-hubspot-forms.php
+++ config/laravel-hubspot-forms.php
@@ -1,8 +1,35 @@
 <?php
 
 return [
-    'laravel_hubspot_forms' => [
-        'access_token' => env('HUBSPOT_ACCESS_TOKEN'),
-        'hubspot_base_url' => env('HUBSPOT_BASE_URL'),
-    ]
+    /*
+    |--------------------------------------------------------------------------
+    | HubSpot Access Token
+    |--------------------------------------------------------------------------
+    | This is the access token for your HubSpot account. This can be generated
+    | in your HubSpot account settings.
+    |
+    */
+    'access_token' => env('HUBSPOT_ACCESS_TOKEN'),
+    /*
+    |--------------------------------------------------------------------------
+    | HubSpot Base URL
+    |--------------------------------------------------------------------------
+    | This is the base URL for the HubSpot API.
+    | This is usually https://api.hubapi.com
+    |
+    */
+    'hubspot_base_url' => env('HUBSPOT_BASE_URL'),
+    /*
+    |--------------------------------------------------------------------------
+    | HubSpot Contact Fields
+    |--------------------------------------------------------------------------
+    | These are the fields that are required to create or update a contact
+    | using the HubSpot API
+    |
+    */
+    'hubspot_contact_fields' => [
+        'email',
+        'firstname',
+        'lastname',
+    ],
 ];
